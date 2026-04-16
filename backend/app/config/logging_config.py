baa17b963cdff46382add9ab053e8a063f387d71
avAs zAde <abas.zade@outlook.com>
2026-04-16 17:33:11 +0330

    refactor logger.

--- backend/app/config/logging_config.py
+++ backend/app/config/logging_config.py
@@ -1,25 +1,10 @@
 import logging
-from logging.config import dictConfig
 
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-        }
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "default",
-        }
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
-}
+def setup_logger(level: str):
+    logging.basicConfig(
+        level=getattr(logging, level.upper(), logging.INFO),
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    )
 
-
-def setup_logging() -> None:
-    dictConfig(LOGGING_CONFIG)
-    logging.getLogger(__name__).debug("Logging configured")
+    return logging.getLogger("uvicorn.error")
--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,51 @@
+from contextlib import asynccontextmanager
+from fastapi_offline import FastAPIOffline
 from fastapi import FastAPI
-
 from app.api.v1.router import api_router
 from app.config.cors import setup_cors
-from app.config.logging_config import setup_logging
+from app.config.logging_config import setup_logger
 from app.config.settings import get_settings
 from app.core.exceptions import register_exception_handlers
 from app.core.middlewares import register_middlewares
 
 
+# -------------------------------------------------------------
+# Settings
+# -------------------------------------------------------------
 settings = get_settings()
-setup_logging()
 
-app = FastAPI(
+
+# -------------------------------------------------------------
+# Logger
+# -------------------------------------------------------------
+logger = setup_logger(level=settings.log_level)
+
+
+# -------------------------------------------------------------
+# Lifespan
+# -------------------------------------------------------------
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("App starting...")
+
+    yield
+
+    print("App shouting down...")
+
+
+# -------------------------------------------------------------
+# Create App instance
+# -------------------------------------------------------------
+app = FastAPIOffline(
     title=settings.app_name,
     version=settings.app_version,
-    docs_url="/docs",
-    redoc_url="/redoc",
+    debug=settings.debug,
+    openapi_url=settings.openapi_url,
+    docs_url=settings.docs_url,
+    redoc_url=settings.redoc_url,
+    lifespan=lifespan,
 )
+
 
 setup_cors(app)
 register_middlewares(app)
