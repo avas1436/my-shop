32cc4d7998b6769ed6cc4a1d2c0f174e0bd8c36e
avAs zAde <abas.zade@outlook.com>
2026-04-22 21:57:46 +0330

    refactor(backend): change user database for OTP and JWT base auth.

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -74,7 +74,7 @@ class Settings(BaseSettings):
     refresh_token_expire_days: int = 7
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=".env.dev",
         # env_file=".env.dev",
         env_file_encoding="utf-8",
         case_sensitive=False,
--- backend/app/modules/users/models.py
+++ backend/app/modules/users/models.py
@@ -1,15 +1,55 @@
-from sqlalchemy import String
+from datetime import date, datetime
+from typing import Optional
+from sqlalchemy import Boolean, Date, DateTime, Enum, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column
-
 from app.common.enums import UserRole
 from app.core.database import Base
 
 
+# ==============================================================================
+# User Model
+# ==============================================================================
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
-    role: Mapped[UserRole] = mapped_column(default=UserRole.CUSTOMER)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    first_name: Mapped[Optional[str]] = mapped_column(
+        String(50), default=None, nullable=False
+    )
+    last_name: Mapped[Optional[str]] = mapped_column(
+        String(50), default=None, nullable=False
+    )
+
+    phone_number: Mapped[str] = mapped_column(
+        String(20),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+    is_phone_verified = mapped_column(Boolean, default=False)
+
+    birth_date: Mapped[Optional[date]] = mapped_column(Date)
+
+    hashed_password: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole, name="UserRole"), default=UserRole.CUSTOMER, nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    last_login: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
+
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), nullable=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True),
+        onupdate=func.now(),
+    )
