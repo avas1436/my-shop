4b422d8ae6033874ee4c27ee463e47570d074d36
avAs zAde <abas.zade@outlook.com>
2026-04-25 15:51:12 +0330

    uncomment send sms

--- backend/app/modules/users/routers.py
+++ backend/app/modules/users/routers.py
@@ -8,8 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
 from app.core.jwt import get_current_user
 from app.core.security import hash_password
-
-# from app.core.sms_service import send_sms
+from app.core.sms_service import send_sms
 from app.modules.users.models import User
 from app.modules.users.schemas import (
     OTPVerify,
@@ -56,13 +55,13 @@ async def request_otp(
                 detail=f"please wait for {wait} seconds",
             )
 
-        # background.add_task(
-        #     send_sms,
-        #     receptor=data.phone_number,
-        #     code=code,
-        # )
+        background.add_task(
+            send_sms,
+            receptor=data.phone_number,
+            code=code,
+        )
 
-        print(code)
+        # print(code)
 
         return {"message": "OTP sent successfully"}
 
