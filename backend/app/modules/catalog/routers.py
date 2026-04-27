c2c9e629a53a7e306e7a01f2265b481cb3e0d3e4
avAs zAde <abas.zade@outlook.com>
2026-04-27 18:39:15 +0330

    feat(backend): add admin create draft product in router with admin onle auth.

--- backend/app/modules/catalog/routers.py
+++ backend/app/modules/catalog/routers.py
@@ -1,11 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.common.enums import UserRole
+from app.common.admin_only import require_admin
 from app.core.database import get_db
-from app.core.jwt import get_current_user
 from app.modules.catalog.repository.product import AdminProductRepository
 from app.modules.catalog.schemas.product import DraftProductCreate, ProductAdminRead
 from app.modules.catalog.services.product import AdminProductService
@@ -28,22 +27,14 @@ router = APIRouter()
     response_model=ProductAdminRead,
     status_code=status.HTTP_201_CREATED,
 )
-async def admin_create_product(
+async def admin_create_draft_product(
     payload: DraftProductCreate,
     db: Annotated[AsyncSession, Depends(get_db)],
-    current_user: Annotated[User, Depends(get_current_user)],  # JWT guard
+    _: Annotated[User, Depends(require_admin)],
 ) -> ProductAdminRead:
-
-    current, _ = current_user
-
-    if not current.role == UserRole.ADMIN:
-        raise HTTPException(
-            status_code=403,
-            detail="only admin",
-        )
 
     service = AdminProductService(AdminProductRepository(db))
 
-    product = await service.create(payload)
+    product = await service.draft_create(payload)
 
     return ProductAdminRead.model_validate(product)
