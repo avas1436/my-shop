8831c698aa08d59476f4218b472a4f4a8d700a63
avAs zAde <abas.zade@outlook.com>
2026-04-29 22:48:01 +0330

    feat(backend): add attribute router to api.

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -2,6 +2,7 @@
 # from app.modules.inventory.routers import router as inventory_router
 from fastapi import APIRouter
 
+from app.modules.catalog.routers.attribute import router as attribute_router
 from app.modules.catalog.routers.brand import router as brand_router
 from app.modules.catalog.routers.category import router as categories_router
 from app.modules.catalog.routers.image import router as images_router
@@ -16,5 +17,6 @@ api_router.include_router(images_router, prefix="/images", tags=["images"])
 api_router.include_router(brand_router, prefix="/brands", tags=["Brands"])
 api_router.include_router(categories_router, prefix="/categories", tags=["categories"])
 api_router.include_router(tag_router, prefix="/tags", tags=["tags"])
+api_router.include_router(attribute_router, prefix="/attributes", tags=["attribute"])
 # api_router.include_router(inventory_router, prefix="/inventory", tags=["inventory"])
 # api_router.include_router(comments_router, prefix="/comments", tags=["comments"])
