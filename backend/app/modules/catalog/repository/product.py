a79313207d143c51b12764016b55dea08fffd7c2
avAs zAde <abas.zade@outlook.com>
2026-05-07 15:00:38 +0330

    refactor(backend): make porduct repository standard. remove commit and refresh from repository.

--- backend/app/modules/catalog/repository/product.py
+++ backend/app/modules/catalog/repository/product.py
@@ -18,33 +18,23 @@ class AdminProductRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    # =========================================================
-    # Create Product
-    # =========================================================
-    async def create(self, product: Product) -> Product:
-        self.db.add(product)
-        await self.db.commit()
-        await self.db.refresh(product)
-        return product
+    # ---------------------------
+    # Check exist with Product Slug
+    # ---------------------------
+    async def exists_by_slug(self, slug: str) -> bool:
+        result = await self.db.execute(select(exists().where(Product.slug == slug)))
+        return bool(result.scalar())
 
-    # =========================================================
-    # Show all of a Product Data
-    # =========================================================
-    # async def list_all(self) -> list[Product]:
-    #     result = await self.db.execute(
-    #         select(Product)
-    #         .options(
-    #             selectinload(Product.category),
-    #             selectinload(Product.inventory),
-    #             selectinload(Product.comments),
-    #         )
-    #         .order_by(Product.id.desc())
-    #     )
-    #     return list(result.scalars().all())
+    # ---------------------------
+    # Check exist with Product SKU
+    # ---------------------------
+    async def exists_by_sku(self, sku: str) -> bool:
+        result = await self.db.execute(select(exists().where(Product.sku == sku)))
+        return bool(result.scalar())
 
-    # =========================================================
+    # ---------------------------
     # Get a Product by ID
-    # =========================================================
+    # ---------------------------
     async def get_by_id(self, product_id: int) -> Product | None:
         stmt = (
             select(Product)
@@ -82,9 +72,24 @@ class AdminProductRepository:
 
         return product
 
-    # =========================================================
+    # ---------------------------
+    # Create Product
+    # ---------------------------
+    async def create(self, product: Product) -> Product:
+        self.db.add(product)
+
+        return product
+
+    # ---------------------------
+    # Update Product
+    # ---------------------------
+    async def update(self, obj: Product) -> Product:
+        self.db.add(obj)
+        return obj
+
+    # ---------------------------
     # Soft Delete a Product
-    # =========================================================
+    # ---------------------------
     async def soft_delete(self, product_id: int) -> bool:
         result = await self.db.execute(
             update(Product)
@@ -92,27 +97,44 @@ class AdminProductRepository:
             .values(deleted_at=datetime.now(UTC), status=ProductStatus.INACTIVE)
             .execution_options(synchronize_session="fetch")
         )
-        await self.db.commit()
+        # await self.db.commit()
+
         return result.rowcount > 0
 
-    # =========================================================
-    # Check exist of Product Slug
-    # =========================================================
-    async def exists_by_slug(self, slug: str) -> bool:
-        result = await self.db.execute(select(exists().where(Product.slug == slug)))
-        return bool(result.scalar())
-
-    # =========================================================
-    # Check exist of Product SKU
-    # =========================================================
-    async def exists_by_sku(self, sku: str) -> bool:
-        result = await self.db.execute(select(exists().where(Product.sku == sku)))
-        return bool(result.scalar())
-
-    # =========================================================
+    # ---------------------------
     # Hard Delete a Product
-    # =========================================================
+    # ---------------------------
     async def hard_delete(self, product_id: int) -> bool:
+
         result = await self.db.execute(delete(Product).where(Product.id == product_id))
-        await self.db.commit()
+        # await self.db.commit()
+
         return result.rowcount > 0
+
+    # ---------------------------
+    # Unit of Work helpers
+    # ---------------------------
+    async def commit(self):
+        await self.db.commit()
+
+    async def rollback(self):
+        await self.db.rollback()
+
+    async def refresh(self, data: Product):
+        await self.db.refresh(data)
+
+
+# ---------------------------
+# Show all of a Product Data
+# ---------------------------
+# async def list_all(self) -> list[Product]:
+#     result = await self.db.execute(
+#         select(Product)
+#         .options(
+#             selectinload(Product.category),
+#             selectinload(Product.inventory),
+#             selectinload(Product.comments),
+#         )
+#         .order_by(Product.id.desc())
+#     )
+#     return list(result.scalars().all())
