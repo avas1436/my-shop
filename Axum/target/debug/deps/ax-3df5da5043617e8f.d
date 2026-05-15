ac907e5ac86e7dd2243b265bfec4a89f35eba83f
avAs zAde <abas.zade@outlook.com>
2026-05-16 01:06:59 +0330

    remove target file from git.

--- Axum/target/debug/deps/ax-3df5da5043617e8f.d
+++
@@ -1,5 +0,0 @@
-/home/avas/Documents/GIT/shop/Axum/target/debug/deps/ax-3df5da5043617e8f.d: src/main.rs
-
-/home/avas/Documents/GIT/shop/Axum/target/debug/deps/libax-3df5da5043617e8f.rmeta: src/main.rs
-
-src/main.rs:
